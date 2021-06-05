9498efdbd73ec0fc3a0b52fa7cef1d3d642c1643
CocoaPebble <jyz18221816395@gmail.com>
2021-06-05 09:12:54 -0400

    first commit

---
+++ src/components/ImagesData.js
@@ -0,0 +1,26 @@
+import Pic1 from "../images/pic1.jpeg";
+import Pic2 from "../images/pic2.jpeg";
+import Pic3 from "../images/pic3.jpeg";
+import Pic4 from "../images/pic4.jpeg";
+import Pic5 from "../images/pic5.jpeg";
+import Pic6 from "../images/pic6.jpeg";
+
+export const images = [
+  { id: 0, title: "Forest", img: Pic1 },
+  { id: 1, title: "Cinque Terre", img: Pic2 },
+  { id: 2, title: "River", img: Pic3 },
+  { id: 3, title: "Aurora", img: Pic4 },
+  { id: 4, title: "Mountain and Forest", img: Pic5 },
+  { id: 5, title: "Ice Mountain", img: Pic6 },
+];
+
+function getImage() {
+  const photos = [
+    "https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1616234643798-12dc87da67b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
+    "https://images.unsplash.com/photo-1519852476561-ec618b0183ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1338&q=80",
+    "https://images.unsplash.com/photo-1513105857239-405e5224e7ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1616511905964-24ed7935a4f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+    "https://images.unsplash.com/photo-1521911494023-701af6c9579c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1408&q=80",
+  ];
+}
